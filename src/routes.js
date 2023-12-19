4684f15973e7ab766535fb00e8afeba531b17a72
larissasn <s4nlari@gmail.com>
2023-12-19 12:26:28 -0300

    Padronizando meu codigo com Eslint + Prettier Após definier configurações no prettier, eu precisei usar o comando no terminal pra aplicar: yarn prettier --write .

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
-import { Router } from "express"
-const { request, response } = require("./app")
+import { Router } from 'express'
 
 const routes = new Router()
 
 routes.get('/', (request, response) => {
-    return response.json({message: "Hello Word"})
+  return response.json({ message: 'Hello Word' })
 })
 
-export default routes 
+export default routes
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app'
 
+const my_app = 'Olá'
+console.log(my_app)
 app.listen(3000)
